d88c9d69c62e486ba66802fdc62a13733304df9f
Valritehachi <valritehachi@gmail.com>
2024-04-19 11:14:38 -0700

    fina edits

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in tmp holberton
 file { '/tmp/holberton':
 ensure  => file,
-path  => '/tmp/holberton',
-mode  => '0744',
-owner => 'www-data',
-group => 'www-data',
+  path  => '/tmp/holberton',
+  mode  => '0744',
+  owner => 'www-data',
+  group => 'www-data',
 content => 'I love Puppet'
 }
