e277eea70edb288ae55c374e711df9bbdf8eb4ba
Valritehachi <valritehachi@gmail.com>
2024-06-04 03:16:02 -0700

    edits

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,8 @@ import requests
 
 def top_ten(subreddit):
     """ a function that queries the Reddit API and prints the titles."""
-    # Construct the URL for the subreddit's hot posts in JSON format
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-    # Define headers for the HTTP request, including User-Agent
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -20,17 +18,12 @@ def top_ten(subreddit):
         "limit": 10
     }
 
-    # Send a GET request to the subreddit's hot posts page
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    # Check if the response status code indicates a not-found error (404)
     if response.status_code == 404:
         print("None")
         return
 
-    # Parse the JSON response and extract the 'data' section
     results = response.json().get("data")
 
-    # Print the titles of the top 10 hottest posts
     [print(c.get("data").get("title")) for c in results.get("children")]
